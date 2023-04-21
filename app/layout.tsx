583afc29881e5f4c660e54042fbff68605f33974
quocdnp-dipro <quocdnp@dipro.vn>
2023-04-21 23:31:47 +0700

    feat/quoc/setup-project-airbnb

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css'
+import { Nunito } from 'next/font/google'
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb Clone',
+};
+
+const font = Nunito({
+  subsets:['latin'],
+})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  )
+}
